a21912958b8cd28b7573b202f317e4398ba04878
Tim Wilkis <twilkis@hotmail.com>
2016-01-04 01:49:28 -0600

    More Tournament modeling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103065352) do
+ActiveRecord::Schema.define(version: 20160103065336) do
 
   create_table "gamematches", force: :cascade do |t|
     t.integer  "matchnum"
