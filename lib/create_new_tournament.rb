77b4d13f9c818412323fcc099072c75348f82faf
Tim Wilkis <twilkis@sssworld-local.com>
2016-01-22 15:37:17 -0600

    Trying some new stuff

--- lib/create_new_tournament.rb
+++ lib/create_new_tournament.rb
@@ -1,9 +1,31 @@
 def createNewTournament(players, details)
   size = players.size
-  size = 21
+  size = 6
   pseedArr = seeding(size)
-  puts pseedArr.to_s
-  puts players.to_s
+  tournament = Hash.new
+  tournament[:name] = "Testing"
+  tournament[:date] =  "10/12/16"
+  tournament[:description] = "This is a test tournament"
+  tournament[:url] = "http://test.test.com"
+  sets = initializeSets(pseedArr, size)
+  tournament[:sets] = sets
+  puts tournament.to_s
+
+end
+
+def initializeSets(seed, size)
+  setsArr = []
+  winners = Math.log(size, 2).ceil
+  losers = (Math.log(size, 2)).ceil + Math.log((Math.log(size, 2)), 2).ceil
+  puts winners
+  puts losers
+  winners.times do |index|
+    puts index
+  end
+  losers.times do |index|
+    puts index
+  end
+
 end
 
 def seeding(numPlayers)
@@ -25,5 +47,16 @@ def nextLayer(pls)
   return out
 end
 
-hash = { "1" => "p1", "2" => "p2", "3" => "p3", "4" => "p4", "5" => "p5", "6" => "p6" }
+hash = Hash.new
+hash = {
+  "1" => "P1",
+  "2" => "P2",
+  "3" => "P3",
+  "4" => "P4",
+  "5" => "P5",
+  "6" => "P6",
+  "7" => "P7",
+  "8" => "P8"
+}
+
 createNewTournament  hash, "something"
